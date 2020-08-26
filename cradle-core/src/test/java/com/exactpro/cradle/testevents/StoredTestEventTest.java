4f8cc3e00a1dbc28207355e7c2b28f2d0d029e6e
vladimir.panarin <vladimir.panarin@exactprosystems.com>
2020-08-26 13:37:39 +0300

    TH2-154. Unhandled exception on StoreEvent without arguments

--- cradle-core/src/test/java/com/exactpro/cradle/testevents/StoredTestEventTest.java
+++ cradle-core/src/test/java/com/exactpro/cradle/testevents/StoredTestEventTest.java
@@ -1,20 +1,28 @@
-/******************************************************************************
- * Copyright (c) 2009-2020, Exactpro Systems LLC
- * www.exactpro.com
- * Build Software to Test Software
+/*
+ * Copyright 2020-2020 Exactpro (Exactpro Systems Limited)
  *
- * All rights reserved.
- * This is unpublished, licensed software, confidential and proprietary 
- * information which is the property of Exactpro Systems LLC or its licensors.
- ******************************************************************************/
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.exactpro.cradle.testevents;
 
+import java.time.Instant;
 import java.util.UUID;
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.exactpro.cradle.utils.CradleIdException;
@@ -22,6 +30,10 @@ import com.exactpro.cradle.utils.CradleStorageException;
 
 public class StoredTestEventTest
 {
+	private static final StoredTestEventId DUMMY_ID = new StoredTestEventId("123");
+	private static final String DUMMY_NAME = "TestEvent";
+	private static final Instant DUMMY_START_TIMESTAMP = Instant.now();
+	
 	private TestEventBatchToStoreBuilder batchSettingsBuilder;
 	private TestEventToStoreBuilder eventBuilder;
 	private StoredTestEventId batchId;
@@ -44,6 +56,21 @@ public class StoredTestEventTest
 	public void prepareBatch() throws CradleStorageException
 	{
 		batch = new StoredTestEventBatch(batchSettings);
+	}
+	
+	@DataProvider(name = "invalid events")
+	public Object[][] invalidEvents()
+	{
+		return new Object[][]
+				{
+					{eventBuilder.build()},  //Empty event
+					{eventBuilder.id(DUMMY_ID).build()},
+					{eventBuilder.name(DUMMY_NAME).build()},
+					{eventBuilder.startTimestamp(DUMMY_START_TIMESTAMP).build()},
+					{eventBuilder.id(DUMMY_ID).name(DUMMY_NAME).build()},
+					{eventBuilder.id(DUMMY_ID).startTimestamp(DUMMY_START_TIMESTAMP).build()},
+					{eventBuilder.name(DUMMY_NAME).startTimestamp(DUMMY_START_TIMESTAMP).build()}
+				};
 	}
 	
 	
@@ -78,7 +105,9 @@ public class StoredTestEventTest
 	public void parentMustBeSet() throws CradleStorageException
 	{
 		batch.addTestEvent(eventBuilder
-				.id(new StoredTestEventId("X"))
+				.id(DUMMY_ID)
+				.name(DUMMY_NAME)
+				.startTimestamp(DUMMY_START_TIMESTAMP)
 				.build());
 	}
 	
@@ -88,6 +117,8 @@ public class StoredTestEventTest
 		for (int i = 0; i <= StoredTestEventBatch.MAX_EVENTS_NUMBER+1; i++)
 			batch.addTestEvent(eventBuilder
 					.id(new StoredTestEventId(Integer.toString(i)))
+					.name(DUMMY_NAME)
+					.startTimestamp(DUMMY_START_TIMESTAMP)
 					.parentId(batch.getParentId())
 					.build());
 	}
@@ -99,6 +130,8 @@ public class StoredTestEventTest
 		for (int i = 0; i <= (StoredTestEventBatch.MAX_EVENTS_SIZE/content.length)+1; i++)
 			batch.addTestEvent(eventBuilder
 					.id(new StoredTestEventId(Integer.toString(i)))
+					.name(DUMMY_NAME)
+					.startTimestamp(DUMMY_START_TIMESTAMP)
 					.parentId(batch.getParentId())
 					.content(content)
 					.build());
@@ -108,8 +141,8 @@ public class StoredTestEventTest
 	public void duplicateIds() throws CradleIdException, CradleStorageException
 	{
 		StoredTestEventId eventId = new StoredTestEventId("AAA");
-		batch.addTestEvent(eventBuilder.id(eventId).parentId(batch.getParentId()).build());
-		batch.addTestEvent(eventBuilder.id(eventId).parentId(batch.getParentId()).build());
+		batch.addTestEvent(eventBuilder.id(eventId).name(DUMMY_NAME).startTimestamp(DUMMY_START_TIMESTAMP).parentId(batch.getParentId()).build());
+		batch.addTestEvent(eventBuilder.id(eventId).name(DUMMY_NAME).startTimestamp(DUMMY_START_TIMESTAMP).parentId(batch.getParentId()).build());
 	}
 	
 	@Test(expectedExceptions = {CradleStorageException.class}, expectedExceptionsMessageRegExp = ".* 'XXX' .* stored in this batch .*")
@@ -117,15 +150,23 @@ public class StoredTestEventTest
 	{
 		StoredTestEventId parentId = new StoredTestEventId("1");
 		batch.addTestEvent(eventBuilder.id(parentId)
+				.name(DUMMY_NAME)
+				.startTimestamp(DUMMY_START_TIMESTAMP)
 				.parentId(batch.getParentId())
 				.build());
 		batch.addTestEvent(eventBuilder.id(new StoredTestEventId("2"))
+				.name(DUMMY_NAME)
+				.startTimestamp(DUMMY_START_TIMESTAMP)
 				.parentId(batch.getParentId())
 				.build());
 		batch.addTestEvent(eventBuilder.id(new StoredTestEventId("3"))
+				.name(DUMMY_NAME)
+				.startTimestamp(DUMMY_START_TIMESTAMP)
 				.parentId(parentId)
 				.build());
 		batch.addTestEvent(eventBuilder.id(new StoredTestEventId("4"))
+				.name(DUMMY_NAME)
+				.startTimestamp(DUMMY_START_TIMESTAMP)
 				.parentId(new StoredTestEventId("XXX"))
 				.build());
 	}
@@ -135,9 +176,13 @@ public class StoredTestEventTest
 	{
 		StoredTestEventId parentId = new StoredTestEventId("1");
 		batch.addTestEvent(eventBuilder.id(parentId)
+				.name(DUMMY_NAME)
+				.startTimestamp(DUMMY_START_TIMESTAMP)
 				.parentId(batch.getParentId())
 				.build());
 		batch.addTestEvent(eventBuilder.id(new StoredTestEventId("2"))
+				.name(DUMMY_NAME)
+				.startTimestamp(DUMMY_START_TIMESTAMP)
 				.parentId(batch.getId())
 				.build());
 	}
@@ -154,10 +199,14 @@ public class StoredTestEventTest
 	{
 		BatchedStoredTestEvent parentEvent = batch.addTestEvent(eventBuilder
 						.id(new StoredTestEventId("1"))
+						.name(DUMMY_NAME)
+						.startTimestamp(DUMMY_START_TIMESTAMP)
 						.parentId(batch.getParentId())
 						.build()),
 				childEvent = batch.addTestEvent(eventBuilder
 						.id(new StoredTestEventId("2"))
+						.name(DUMMY_NAME)
+						.startTimestamp(DUMMY_START_TIMESTAMP)
 						.parentId(parentEvent.getId())
 						.build());
 		
@@ -168,7 +217,9 @@ public class StoredTestEventTest
 	public void rootIsRoot() throws CradleStorageException
 	{
 		BatchedStoredTestEvent parentEvent = batch.addTestEvent(eventBuilder
-				.id(new StoredTestEventId("1"))
+				.id(DUMMY_ID)
+				.name(DUMMY_NAME)
+				.startTimestamp(DUMMY_START_TIMESTAMP)
 				.parentId(batch.getParentId())
 				.build());
 		Assert.assertEquals(batch.getRootTestEvents().contains(parentEvent), true, "Root event is listed in roots");
@@ -179,13 +230,24 @@ public class StoredTestEventTest
 	{
 		BatchedStoredTestEvent parentEvent = batch.addTestEvent(eventBuilder
 						.id(new StoredTestEventId("1"))
+						.name(DUMMY_NAME)
+						.startTimestamp(DUMMY_START_TIMESTAMP)
 						.parentId(batch.getParentId())
 						.build()),
 				childEvent = batch.addTestEvent(eventBuilder
 						.id(new StoredTestEventId("2"))
+						.name(DUMMY_NAME)
+						.startTimestamp(DUMMY_START_TIMESTAMP)
 						.parentId(parentEvent.getId())
 						.build());
 
 		Assert.assertEquals(batch.getRootTestEvents().contains(childEvent), false, "Child event is not listed in roots");
+	}
+	
+	@Test(dataProvider = "invalid events",
+			expectedExceptions = {CradleStorageException.class})
+	public void eventValidation(TestEventToStore event) throws CradleStorageException
+	{
+		batch.addTestEvent(event);
 	}
 }
