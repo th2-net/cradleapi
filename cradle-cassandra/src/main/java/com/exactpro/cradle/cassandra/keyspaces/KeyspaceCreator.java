283ee49dff5ecf58219b71e09f9e9a485d6de996
georgiano <georgiano@lena>
2022-03-15 12:29:02 +0400

    [TH2-2974] forcing metadata refresh before keyspace existence check

--- cradle-cassandra/src/main/java/com/exactpro/cradle/cassandra/keyspaces/KeyspaceCreator.java
+++ cradle-cassandra/src/main/java/com/exactpro/cradle/cassandra/keyspaces/KeyspaceCreator.java
@@ -17,10 +17,12 @@
 package com.exactpro.cradle.cassandra.keyspaces;
 
 import com.datastax.oss.driver.api.core.CqlIdentifier;
+import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.metadata.schema.KeyspaceMetadata;
 import com.datastax.oss.driver.api.core.metadata.schema.TableMetadata;
 import com.datastax.oss.driver.api.querybuilder.SchemaBuilder;
 import com.datastax.oss.driver.api.querybuilder.schema.CreateKeyspace;
+import com.datastax.oss.driver.api.querybuilder.schema.CreateKeyspaceStart;
 import com.datastax.oss.driver.api.querybuilder.schema.CreateTable;
 import com.exactpro.cradle.cassandra.CassandraStorageSettings;
 import com.exactpro.cradle.cassandra.utils.QueryExecutor;
@@ -33,8 +35,8 @@ import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
-public abstract class KeyspaceCreator
-{
+public abstract class KeyspaceCreator {
+
 	private static final Logger logger = LoggerFactory.getLogger(KeyspaceCreator.class);
 	
 	public static final long KEYSPACE_WAIT_TIMEOUT = 500;
@@ -46,8 +48,7 @@ public abstract class KeyspaceCreator
 
 	private KeyspaceMetadata keyspaceMetadata;
 	
-	public KeyspaceCreator(String keyspace, QueryExecutor queryExecutor, CassandraStorageSettings settings)
-	{
+	public KeyspaceCreator(String keyspace, QueryExecutor queryExecutor, CassandraStorageSettings settings)	{
 		this.keyspace = keyspace;
 		this.queryExecutor = queryExecutor;
 		this.settings = settings;
@@ -57,106 +58,100 @@ public abstract class KeyspaceCreator
 	protected abstract void createTables() throws IOException;
 	
 	
-	public void createAll() throws IOException, CradleStorageException
-	{
+	public void createAll() throws IOException, CradleStorageException {
 		createKeyspace();
 		createTables();
 	}
 	
 	
-	public String getKeyspace()
-	{
+	public String getKeyspace()	{
 		return keyspace;
 	}
-	
-	public QueryExecutor getQueryExecutor()
-	{
+
+
+	public QueryExecutor getQueryExecutor()	{
 		return queryExecutor;
 	}
-	
-	public CassandraStorageSettings getSettings()
-	{
+
+
+	public CassandraStorageSettings getSettings() {
 		return settings;
 	}
 	
 	
-	public void createKeyspace() throws IOException, CradleStorageException
-	{
-		Optional<KeyspaceMetadata> meta = obtainKeyspaceMetadata();
-		if (meta.isPresent())
+	public void createKeyspace() throws IOException, CradleStorageException	{
+		if (obtainKeyspaceMetadata() != null)
 			throw new CradleStorageException("Keyspace '" + keyspace + "' already exists");
 		
 		logger.info("Creating keyspace '{}'", keyspace);
-		CreateKeyspace createKs = settings.getNetworkTopologyStrategy() != null
-				? SchemaBuilder.createKeyspace(keyspace)
-				.withNetworkTopologyStrategy(settings.getNetworkTopologyStrategy().asMap())
-				: SchemaBuilder.createKeyspace(keyspace).withSimpleStrategy(settings.getKeyspaceReplicationFactor());
-		queryExecutor.executeQuery(createKs.asCql(), true);
+		CreateKeyspaceStart createKeyspaceStart = SchemaBuilder.createKeyspace(keyspace);
+		CreateKeyspace createKeyspace = settings.getNetworkTopologyStrategy() != null
+				? createKeyspaceStart.withNetworkTopologyStrategy(settings.getNetworkTopologyStrategy().asMap())
+				: createKeyspaceStart.withSimpleStrategy(settings.getKeyspaceReplicationFactor());
+		queryExecutor.executeQuery(createKeyspace.asCql(), true);
 		logger.info("Keyspace '{}' has been created", keyspace);
 		
 		awaitKeyspaceReady();
 	}
-	
-	private void awaitKeyspaceReady() throws CradleStorageException
-	{
+
+
+	private void awaitKeyspaceReady() throws CradleStorageException	{
 		int attempt = 0;
 		long timeRemaining = keyspaceReadinessTimeout;
 
-		while(getKeyspaceMetadata() == null && timeRemaining > 0)
-		{
+		while (obtainKeyspaceMetadata() == null && timeRemaining > 0) {
 			attempt++;
 			logger.debug("[{}] attempt to wait {}ms for the readiness of the keyspace", attempt, KEYSPACE_WAIT_TIMEOUT);
-			try
-			{
+			try	{
 				TimeUnit.MILLISECONDS.sleep(Math.min(KEYSPACE_WAIT_TIMEOUT, timeRemaining));
 				timeRemaining -= KEYSPACE_WAIT_TIMEOUT;
-			}
-			catch (InterruptedException e)
-			{
+			} catch (InterruptedException e) {
 				throw new CradleStorageException(
 						"Waiting for keyspace '" + keyspace + "' readiness has been interrupted", e);
 			}
 		}
 		
-		if (getKeyspaceMetadata() == null)
+		if (obtainKeyspaceMetadata() == null)
 			throw new CradleStorageException(
 					"Keyspace '" + keyspace + "' unavailable after " + keyspaceReadinessTimeout + "ms of awaiting");
 	}
-	
-	protected boolean isTableExists(String tableName)
-	{
+
+
+	protected boolean isTableExists(String tableName) {
 		return keyspaceMetadata.getTable(tableName).isPresent();
 	}
-	
-	protected boolean isIndexExists(String indexName, String tableName)
-	{
+
+
+	protected boolean isIndexExists(String indexName, String tableName) {
 		Optional<TableMetadata> tableMetadata = keyspaceMetadata.getTable(tableName);
 		return tableMetadata.isPresent() && tableMetadata.get().getIndexes().containsKey(CqlIdentifier.fromCql(indexName));
 	}
-	
-	protected KeyspaceMetadata getKeyspaceMetadata()
-	{
+
+
+	protected KeyspaceMetadata getKeyspaceMetadata() {
 		if (keyspaceMetadata != null)
 			return keyspaceMetadata;
 		
-		obtainKeyspaceMetadata().ifPresent(value -> keyspaceMetadata = value);
+		obtainKeyspaceMetadata();
 		return keyspaceMetadata;
 	}
-	
-	private Optional<KeyspaceMetadata> obtainKeyspaceMetadata()
-	{
-		return queryExecutor.getSession().getMetadata().getKeyspace(keyspace);
+
+
+	private KeyspaceMetadata obtainKeyspaceMetadata()	{
+		CqlSession session = queryExecutor.getSession();
+		session.refreshSchema();
+		keyspaceMetadata = session.getMetadata().getKeyspace(keyspace).orElse(null);
+		return keyspaceMetadata;
 	}
-	
-	protected boolean isColumnExists(String tableName, String columnName)
-	{
+
+
+	protected boolean isColumnExists(String tableName, String columnName) {
 		return keyspaceMetadata.getTable(tableName).get().getColumn(columnName).isPresent();
 	}
-	
-	protected void createTable(String tableName, Supplier<CreateTable> query) throws IOException
-	{
-		if (isTableExists(tableName))
-		{
+
+
+	protected void createTable(String tableName, Supplier<CreateTable> query) throws IOException {
+		if (isTableExists(tableName)){
 			logger.info("Table '{}' already exists", tableName);
 			return;
 		}
@@ -165,11 +160,10 @@ public abstract class KeyspaceCreator
 		queryExecutor.executeQuery(query.get().asCql(), true);
 		logger.info("Table '{}' has been created", tableName);
 	}
-	
-	protected void createIndex(String indexName, String tableName, String columnName) throws IOException
-	{
-		if (isIndexExists(indexName, tableName))
-		{
+
+
+	protected void createIndex(String indexName, String tableName, String columnName) throws IOException {
+		if (isIndexExists(indexName, tableName)) {
 			logger.info("Index '{}' already exists", indexName);
 			return;
 		}
