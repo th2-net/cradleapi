d426698209f7bf347d350e1d38f6cb0b7bf2e85b
georgiano <georgiano@lena>
2022-03-22 12:30:20 +0400

    [TH2-3363] StatisticsWorker refactoring

--- cradle-cassandra/src/main/java/com/exactpro/cradle/cassandra/workers/StatisticsWorker.java
+++ cradle-cassandra/src/main/java/com/exactpro/cradle/cassandra/workers/StatisticsWorker.java
@@ -20,16 +20,14 @@ import com.exactpro.cradle.BookId;
 import com.exactpro.cradle.Counter;
 import com.exactpro.cradle.EntityType;
 import com.exactpro.cradle.FrameType;
-import com.exactpro.cradle.cassandra.counters.CounterCache;
-import com.exactpro.cradle.cassandra.counters.EntityStatisticsCollector;
-import com.exactpro.cradle.cassandra.counters.MessageStatisticsCollector;
-import com.exactpro.cradle.cassandra.counters.TimeFrameCounter;
+import com.exactpro.cradle.cassandra.counters.*;
 import com.exactpro.cradle.cassandra.dao.CradleOperators;
 import com.exactpro.cradle.serialization.SerializedEntityMetadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -42,18 +40,15 @@ public class StatisticsWorker implements Runnable, EntityStatisticsCollector, Me
 
     private final CradleOperators ops;
     private final long interval;
-    private final BookEntityCounterCache bookEntityCounterCache;
-    private final BookMessageCounterCache bookMessageCounterCache;
+    private final Map<BookId, BookCounterCaches> bookCounterCaches;
     private final Function<BoundStatementBuilder, BoundStatementBuilder> writeAttrs;
 
     public StatisticsWorker(CradleOperators ops, Function<BoundStatementBuilder, BoundStatementBuilder> writeAttrs, long persistanceInterval) {
         this.ops = ops;
         this.writeAttrs = writeAttrs;
         this.interval = persistanceInterval;
-        this.bookMessageCounterCache = new BookMessageCounterCache();
-        this.bookEntityCounterCache = new BookEntityCounterCache();
+        this.bookCounterCaches = new ConcurrentHashMap<>();
     }
-
 
     private ScheduledExecutorService executorService;
     public void start() {
@@ -86,46 +81,41 @@ public class StatisticsWorker implements Runnable, EntityStatisticsCollector, Me
     }
 
 
-    private EntityCounterCache createEntityCounters() {
-        EntityCounterCache entityCounters = new EntityCounterCache();
-        for (EntityType t : EntityType.values())
-            entityCounters.put(t, new CounterCache());
-        return entityCounters;
+    private BookCounterCaches getBookCounterCaches(BookId bookId) {
+        return bookCounterCaches.computeIfAbsent(bookId, k -> new BookCounterCaches(bookId));
     }
+
+
+    private void updateCounters(CounterCache counters, Collection<SerializedEntityMetadata> batchMetadata) {
+        batchMetadata.forEach(meta -> {
+            Counter counter = new Counter(1, meta.getSerializedEntitySize());
+            for (FrameType t : FrameType.values()) {
+                counters.getCounterSamples(t).update(meta.getTimestamp(), counter);
+            }
+        });
+    }
+
 
     @Override
     public void updateEntityBatchStatistics(BookId bookId, EntityType entityType, Collection<SerializedEntityMetadata> batchMetadata) {
-
-        EntityCounterCache entityCounters = bookEntityCounterCache.computeIfAbsent(bookId, k -> createEntityCounters());
-        CounterCache counters = entityCounters.get(entityType);
-        batchMetadata.forEach(meta -> {
-            Counter counter = new Counter(1, meta.getSerializedEntitySize());
-            for (FrameType t : FrameType.values()) {
-                counters.getCounterSamples(t).update(meta.getTimestamp(), counter);
-            }
-        });
+        CounterCache counters = getBookCounterCaches(bookId)
+                .getEntityCounterCache()
+                .forEntityType(entityType);
+        updateCounters(counters, batchMetadata);
     }
 
-    private MessageCounterCache createMessageCounters() {
-        return new MessageCounterCache();
-    }
 
     @Override
     public void updateMessageBatchStatistics(BookId bookId, String sessionAlias, String direction, Collection<SerializedEntityMetadata> batchMetadata) {
-
-        MessageCounterCache messageCounters = bookMessageCounterCache.computeIfAbsent(bookId, k -> createMessageCounters());
-        MessageKey key = new MessageKey(sessionAlias, direction);
-        CounterCache counters = messageCounters.get(key);
-        batchMetadata.forEach(meta -> {
-            Counter counter = new Counter(1, meta.getSerializedEntitySize());
-            for (FrameType t : FrameType.values()) {
-                counters.getCounterSamples(t).update(meta.getTimestamp(), counter);
-            }
-        });
+        CounterCache counters = getBookCounterCaches(bookId)
+                .getMessageCounterCache()
+                .get(new BookCounterCaches.MessageKey(sessionAlias, direction));
+        updateCounters(counters, batchMetadata);
 
         // update entity statistics separately
         updateEntityBatchStatistics(bookId, EntityType.MESSAGE, batchMetadata);
     }
+
 
     private void persistEntityCounters(BookId bookId, EntityType entityType, FrameType frameType, TimeFrameCounter counter) {
         try {
@@ -140,13 +130,13 @@ public class StatisticsWorker implements Runnable, EntityStatisticsCollector, Me
             );
         } catch (Exception e) {
             logger.error("Exception persisting counter, retry scheduled", e);
-            EntityCounterCache entityCounters = bookEntityCounterCache.get(bookId);
-            entityCounters.get(entityType).getCounterSamples(frameType).update(counter.getFrameStart(), counter.getCounter());
+            BookCounterCaches.EntityCounterCache entityCounters = getBookCounterCaches(bookId).getEntityCounterCache();
+            entityCounters.forEntityType(entityType).getCounterSamples(frameType).update(counter.getFrameStart(), counter.getCounter());
         }
     }
 
 
-    private void persistMessageCounters(BookId bookId, MessageKey key, FrameType frameType, TimeFrameCounter counter) {
+    private void persistMessageCounters(BookId bookId, BookCounterCaches.MessageKey key, FrameType frameType, TimeFrameCounter counter) {
         try {
             logger.trace("Persisting counter for {}:{}:{}:{}:{}", bookId, key.getSessionAlias(), key.getDirection(), frameType, counter.getFrameStart());
             ops.getOperators(bookId).getMessageStatisticsOperator().update(
@@ -160,8 +150,8 @@ public class StatisticsWorker implements Runnable, EntityStatisticsCollector, Me
             );
         } catch (Exception e) {
             logger.error("Exception persisting counter, retry scheduled", e);
-            MessageCounterCache entityCounters = bookMessageCounterCache.get(bookId);
-            entityCounters.get(key).getCounterSamples(frameType).update(counter.getFrameStart(), counter.getCounter());
+            BookCounterCaches.MessageCounterCache messageCounters = getBookCounterCaches(bookId).getMessageCounterCache();
+            messageCounters.get(key).getCounterSamples(frameType).update(counter.getFrameStart(), counter.getCounter());
         }
     }
 
@@ -170,21 +160,23 @@ public class StatisticsWorker implements Runnable, EntityStatisticsCollector, Me
         logger.trace("executing StatisticsWorker job");
 
         try {
-            for (BookId bookId: bookEntityCounterCache.keySet()) {
+            for (BookId bookId: bookCounterCaches.keySet()) {
+
+                BookCounterCaches bookCounterCaches = getBookCounterCaches(bookId);
 
                 // persist all cached entity counters
-                EntityCounterCache entityCounters = bookEntityCounterCache.get(bookId);
+                BookCounterCaches.EntityCounterCache entityCounters = bookCounterCaches.getEntityCounterCache();
                 for (FrameType frameType : FrameType.values()) {
                     for (EntityType entityType : EntityType.values()) {
-                        Collection<TimeFrameCounter> counters = entityCounters.get(entityType).getCounterSamples(frameType).extractAll();
+                        Collection<TimeFrameCounter> counters = entityCounters.forEntityType(entityType).getCounterSamples(frameType).extractAll();
                         counters.forEach(counter -> persistEntityCounters(bookId, entityType, frameType, counter));
                     }
                 }
 
                 // persist all cached message counters
-                MessageCounterCache messageCounters = bookMessageCounterCache.get(bookId);
-                Collection<MessageKey> messageKeys = messageCounters.keys();
-                for (MessageKey key : messageKeys) {
+                BookCounterCaches.MessageCounterCache messageCounters = bookCounterCaches.getMessageCounterCache();
+                Collection<BookCounterCaches.MessageKey> messageKeys = messageCounters.messageKeys();
+                for (BookCounterCaches.MessageKey key : messageKeys) {
                     CounterCache counterCache = messageCounters.extract(key);
                     if (counterCache == null)
                         continue;
@@ -199,62 +191,5 @@ public class StatisticsWorker implements Runnable, EntityStatisticsCollector, Me
         }
 
         logger.trace("StatisticsWorker job complete");
-    }
-
-    private static class MessageCounterCache {
-        private Map<MessageKey, CounterCache> cache = new HashMap<>();
-        synchronized void put(MessageKey key, CounterCache counters) {
-            cache.put(key, counters);
-        }
-        synchronized CounterCache get(MessageKey key) {
-            return cache.computeIfAbsent(key, k -> new CounterCache());
-        }
-        synchronized CounterCache extract(MessageKey key) {
-            CounterCache result = cache.get(key);
-            cache.remove(key);
-            return result;
-        }
-        synchronized Collection<MessageKey> keys() {
-            return new HashSet<>(cache.keySet());
-        }
-    }
-
-    private static class EntityCounterCache extends ConcurrentHashMap<EntityType, CounterCache> {}
-    private static class BookMessageCounterCache extends ConcurrentHashMap<BookId, MessageCounterCache> {}
-    private static class BookEntityCounterCache extends ConcurrentHashMap<BookId, EntityCounterCache> {}
-    private static class MessageKey {
-        private final String sessionAlias;
-        private final String direction;
-        public MessageKey(String sessionAlias, String direction) {
-            this.sessionAlias = sessionAlias;
-            this.direction = direction;
-        }
-
-        public String getSessionAlias() {
-            return sessionAlias;
-        }
-
-        public String getDirection() {
-            return direction;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof MessageKey)) return false;
-
-            MessageKey that = (MessageKey) o;
-
-            if (!Objects.equals(sessionAlias, that.sessionAlias))
-                return false;
-            return Objects.equals(direction, that.direction);
-        }
-
-        @Override
-        public int hashCode() {
-            int result = sessionAlias != null ? sessionAlias.hashCode() : 0;
-            result = 31 * result + (direction != null ? direction.hashCode() : 0);
-            return result;
-        }
     }
 }
