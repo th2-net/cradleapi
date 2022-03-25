2821ffef701df90c70ff554ef59a2f694830555b
georgiano <georgiano@lena>
2022-03-25 11:53:58 +0400

    [TH2-3365] Test refactoring example

--- cradle-cassandra/src/test/java/com/exactpro/cradle/cassandra/CassandraCradleStorageTest.java
+++ cradle-cassandra/src/test/java/com/exactpro/cradle/cassandra/CassandraCradleStorageTest.java
@@ -8,6 +8,8 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.testng.Assert.*;
+
 public class CassandraCradleStorageTest {
 
     @Test
@@ -20,7 +22,7 @@ public class CassandraCradleStorageTest {
         actual = CassandraCradleStorage.sliceInterval(start, end);
         expected = new ArrayList<>();
 
-        assert actual.equals(expected);
+        assertEquals(actual, expected);
     }
 
     @Test
@@ -39,7 +41,7 @@ public class CassandraCradleStorageTest {
                 Instant.parse("2022-03-22T17:05:38.000Z"),
                 Instant.parse("2022-03-22T17:05:38.500Z")));
 
-        assert actual.equals(expected);
+        assertEquals(actual, expected);
     }
 
     @Test
@@ -58,7 +60,7 @@ public class CassandraCradleStorageTest {
                 Instant.parse("2022-03-22T17:06:00.000Z"),
                 Instant.parse("2022-03-22T17:06:59.000Z")));
 
-        assert actual.equals(expected);
+        assertEquals(actual, expected);
     }
 
     @Test
@@ -77,7 +79,7 @@ public class CassandraCradleStorageTest {
                 Instant.parse("2022-03-22T18:00:00.000Z"),
                 Instant.parse("2022-03-22T18:23:00.000Z")));
 
-        assert actual.equals(expected);
+        assertEquals(actual, expected);
     }
 
     @Test
@@ -93,7 +95,7 @@ public class CassandraCradleStorageTest {
                 Instant.parse("2022-03-22T17:00:00.000Z"),
                 Instant.parse("2022-03-24T20:00:00.000Z")));
 
-        assert actual.equals(expected);
+        assertEquals(actual, expected);
     }
 
     @Test
@@ -115,7 +117,7 @@ public class CassandraCradleStorageTest {
                 Instant.parse("2022-03-22T17:05:40.000Z"),
                 Instant.parse("2022-03-22T17:05:40.200Z")));
 
-        assert actual.equals(expected);
+        assertEquals(actual, expected);
     }
 
     @Test
@@ -143,40 +145,56 @@ public class CassandraCradleStorageTest {
                 Instant.parse("2022-03-22T17:15:40.000Z"),
                 Instant.parse("2022-03-22T17:15:40.200Z")));
 
-        assert actual.equals(expected);
+        assertEquals(actual, expected);
     }
 
     @Test
     public void testUptoHourFramesInterval () {
-        Instant start, end;
-        start = Instant.parse("2022-03-22T17:05:37.128Z");
-        end  = Instant.parse("2022-03-22T20:15:40.250Z");
 
-        List<ImmutableTriple<FrameType, Instant, Instant>> actual, expected;
-        actual = CassandraCradleStorage.sliceInterval(start, end);
-        expected = new ArrayList<>();
-        expected.add(new ImmutableTriple<>(FrameType.TYPE_100MS,
-                Instant.parse("2022-03-22T17:05:37.100Z"),
-                Instant.parse("2022-03-22T17:05:38.000Z")));
-        expected.add(new ImmutableTriple<>(FrameType.TYPE_SECOND,
-                Instant.parse("2022-03-22T17:05:38.000Z"),
-                Instant.parse("2022-03-22T17:06:00.000Z")));
-        expected.add(new ImmutableTriple<>(FrameType.TYPE_MINUTE,
-                Instant.parse("2022-03-22T17:06:00.000Z"),
-                Instant.parse("2022-03-22T18:00:00.000Z")));
-        expected.add(new ImmutableTriple<>(FrameType.TYPE_HOUR,
-                Instant.parse("2022-03-22T18:00:00.000Z"),
-                Instant.parse("2022-03-22T20:00:00.000Z")));
-        expected.add(new ImmutableTriple<>(FrameType.TYPE_MINUTE,
-                Instant.parse("2022-03-22T20:00:00.000Z"),
-                Instant.parse("2022-03-22T20:15:00.000Z")));
-        expected.add(new ImmutableTriple<>(FrameType.TYPE_SECOND,
-                Instant.parse("2022-03-22T20:15:00.000Z"),
-                Instant.parse("2022-03-22T20:15:40.000Z")));
-        expected.add(new ImmutableTriple<>(FrameType.TYPE_100MS,
-                Instant.parse("2022-03-22T20:15:40.000Z"),
-                Instant.parse("2022-03-22T20:15:40.200Z")));
+        final Instant start = Instant.parse("2022-03-22T17:05:37.128Z");
+        final Instant end   = Instant.parse("2022-03-22T20:15:40.250Z");
 
-        assert actual.equals(expected);
+        final List<ImmutableTriple<FrameType, Instant, Instant>> actual = CassandraCradleStorage.sliceInterval(start, end);
+        validateSlices(actual, start, end, 7);
+    }
+
+
+    private void validateSlices(List<ImmutableTriple<FrameType, Instant, Instant>> actual,
+                                Instant start,
+                                Instant end,
+                                int expectedSize) {
+
+        final int size = actual.size();
+        final FrameType minFrame = FrameType.values()[0];
+
+        assertEquals(actual.get(0).getMiddle(), minFrame.getFrameStart(start), "Wrong start time of the series:");
+        assertEquals(actual.get(size - 1).getRight(), minFrame.getFrameEnd(end), "Wrong end time of the series:");
+        assertEquals(size, expectedSize, "Wrong number of intervals:");
+        validateContinuity(actual);
+    }
+
+
+    private void validateInterval(ImmutableTriple<FrameType, Instant, Instant> interval) {
+        FrameType frameType = interval.getLeft();
+        Instant start = interval.getMiddle();
+        Instant end = interval.getRight();
+        if (!start.isBefore(end))
+            fail(String.format("Interval start must be strictly before its end: start=%s, end=%s", start, end));
+
+        assertEquals(start, frameType.getFrameStart(start), String.format("Interval start does not align to FrameType.%s start:", frameType));
+        assertEquals(start, frameType.getFrameStart(start), String.format("Interval end does not align to FrameType.%s end:", frameType));
+    }
+
+
+    private void validateContinuity(List<ImmutableTriple<FrameType, Instant, Instant>> intervals) {
+        ImmutableTriple<FrameType, Instant, Instant> last = null;
+        for (ImmutableTriple<FrameType, Instant, Instant> el : intervals) {
+            validateInterval(el);
+            if (last != null) {
+                assertNotEquals(el.getLeft(), last.getLeft(), String.format("Identical frame types[%s] on adjacent elements", el.getLeft()));
+                assertEquals(el.getMiddle(), last.getRight(), "Sequence continuity failure:");
+            }
+            last = el;
+        }
     }
 }
