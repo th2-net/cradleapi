157d0ef70b019eabe8450a968931609c1ab3dcf0
georgiano <georgiano@lena>
2022-07-11 13:39:58 +0400

    [TH2-3191] extracting grpc protocol version from metadata

--- cradle-core/src/main/java/com/exactpro/cradle/messages/StoredMessage.java
+++ cradle-core/src/main/java/com/exactpro/cradle/messages/StoredMessage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2021 Exactpro (Exactpro Systems Limited)
+ * Copyright 2020-2022 Exactpro (Exactpro Systems Limited)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,7 @@ package com.exactpro.cradle.messages;
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.Arrays;
+import java.util.Objects;
 
 import com.exactpro.cradle.Direction;
 import com.exactpro.cradle.utils.CompressionUtils;
@@ -26,33 +27,31 @@ import com.exactpro.cradle.utils.CompressionUtils;
 /**
  * Holds information about one message stored in Cradle.
  */
-public class StoredMessage implements Serializable
-{
+public class StoredMessage implements Serializable {
+
 	private static final long serialVersionUID = 200983136307497672L;
+	public static final String METADATA_KEY_PROTOCOL = "com.exactpro.th2.cradle.grpc.protocol";
 	
 	private final StoredMessageId id;
 	private final Instant timestamp;
 	private final StoredMessageMetadata metadata;
 	private final byte[] content;
-	
-	public StoredMessage(MessageToStore message, StoredMessageId id)
-	{
+
+	public StoredMessage(MessageToStore message, StoredMessageId id) {
 		this.id = id;
 		this.timestamp = message.getTimestamp();
 		this.metadata = message.getMetadata() != null ? new StoredMessageMetadata(message.getMetadata()) : null;
 		this.content = message.getContent();
 	}
 	
-	public StoredMessage(StoredMessage copyFrom, StoredMessageId id)
-	{
+	public StoredMessage(StoredMessage copyFrom, StoredMessageId id) {
 		this.id = id;
 		this.timestamp = copyFrom.getTimestamp();
 		this.metadata = copyFrom.getMetadata() != null ? new StoredMessageMetadata(copyFrom.getMetadata()) : null;
 		this.content = copyFrom.getContent();
 	}
 	
-	public StoredMessage(StoredMessage copyFrom)
-	{
+	public StoredMessage(StoredMessage copyFrom) {
 		this(copyFrom, copyFrom.getId());
 	}
 
@@ -119,56 +118,39 @@ public class StoredMessage implements Serializable
 	{
 		return content;
 	}
-	
-	
-	@Override
-	public int hashCode()
-	{
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + Arrays.hashCode(content);
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((metadata == null) ? 0 : metadata.hashCode());
-		result = prime * result + ((timestamp == null) ? 0 : timestamp.hashCode());
-		return result;
+
+	/**
+	 * @return grpc protocol
+	 */
+	public String getProtocol() {
+		return metadata == null ? null : metadata.get(METADATA_KEY_PROTOCOL);
 	}
-	
+
+
 	@Override
-	public boolean equals(Object obj)
-	{
-		if (this == obj)
+	public int hashCode() {
+		return Objects.hash(content, id, metadata, timestamp);
+	}
+
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
 			return true;
-		if (obj == null)
+		if (! (o instanceof StoredMessage))
 			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		StoredMessage other = (StoredMessage) obj;
-		if (!Arrays.equals(content, other.content))
-			return false;
-		if (id == null)
-		{
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (metadata == null)
-		{
-			if (other.metadata != null)
-				return false;
-		} else if (!metadata.equals(other.metadata))
-			return false;
-		if (timestamp == null)
-		{
-			if (other.timestamp != null)
-				return false;
-		} else if (!timestamp.equals(other.timestamp))
-			return false;
-		return true;
+
+		StoredMessage other = (StoredMessage) o;
+		return 	Objects.equals(id, other.id) &&
+				Objects.equals(metadata, other.metadata) &&
+				Objects.equals(timestamp, other.timestamp) &&
+				Arrays.equals(content, other.content);
 	}
-	
+
+
 	@Override
-	public String toString()
-	{
+	public String toString() {
+
 		return new StringBuilder()
 				.append("StoredMessage{").append(CompressionUtils.EOL)
 				.append("ID=").append(id).append(",").append(CompressionUtils.EOL)
