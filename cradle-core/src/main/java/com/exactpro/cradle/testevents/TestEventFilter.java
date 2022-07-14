a315ae56f238f0648c89e030bdbe809e3761d074
georgiano <15014186+georgiano@users.noreply.github.com>
2022-07-14 14:46:30 +0400

    [TH2-3892] added event id and order to test event filter

--- cradle-core/src/main/java/com/exactpro/cradle/testevents/TestEventFilter.java
+++ cradle-core/src/main/java/com/exactpro/cradle/testevents/TestEventFilter.java
@@ -16,138 +16,139 @@
 
 package com.exactpro.cradle.testevents;
 
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.exactpro.cradle.filters.AbstractFilter;
-import org.apache.commons.lang3.StringUtils;
-
 import com.exactpro.cradle.BookId;
+import com.exactpro.cradle.Order;
 import com.exactpro.cradle.PageId;
+import com.exactpro.cradle.filters.AbstractFilter;
 import com.exactpro.cradle.filters.FilterForGreater;
 import com.exactpro.cradle.filters.FilterForLess;
 import com.exactpro.cradle.utils.CradleStorageException;
+import org.apache.commons.lang3.StringUtils;
 
-public class TestEventFilter extends AbstractFilter
-{
-	private final String scope;
+import java.time.Instant;
+
+public class TestEventFilter extends AbstractFilter {
+    private final String scope;
+	private StoredTestEventId id;
 	private StoredTestEventId parentId;
-	private boolean root;
+    private boolean root;
 
-	public TestEventFilter(BookId bookId, String scope, PageId pageId) throws CradleStorageException
-	{
-		super(bookId, pageId);
-		this.scope = scope;
-		validate();
-	}
-	
-	public TestEventFilter(BookId bookId, String scope) throws CradleStorageException
-	{
-		this(bookId, scope, null);
-	}
-	
-	public TestEventFilter(TestEventFilter copyFrom) throws CradleStorageException
-	{
-		super(copyFrom);
-		this.scope = copyFrom.getScope();
+    public TestEventFilter(BookId bookId, String scope, PageId pageId) throws CradleStorageException {
+        super(bookId, pageId);
+        this.scope = scope;
+        validate();
+    }
 
-		//User can specify parentId or root=true or omit both to get all events, whatever the parent. No way to filter "all non-root events"
-		if (copyFrom.isRoot())
-			setRoot();
-		else
-			setParentId(copyFrom.getParentId());
-		
-		validate();
-	}
-	
-	
-	public static TestEventFilterBuilder builder()
-	{
-		return new TestEventFilterBuilder();
-	}
-	
-	public String getScope()
-	{
-		return scope;
-	}
+    public TestEventFilter(BookId bookId, String scope) throws CradleStorageException {
+        this(bookId, scope, null);
+    }
+
+    public TestEventFilter(TestEventFilter copyFrom) throws CradleStorageException {
+        super(copyFrom);
+        this.scope = copyFrom.getScope();
+        this.id = copyFrom.getId();
+
+        //User can specify parentId or root=true or omit both to get all events, whatever the parent. No way to filter "all non-root events"
+        if (copyFrom.isRoot())
+            setRoot();
+        else
+            setParentId(copyFrom.getParentId());
+
+        validate();
+    }
 
 
-	public FilterForGreater<Instant> getStartTimestampFrom()
-	{
-		return super.getFrom();
+    public static TestEventFilterBuilder builder() {
+        return new TestEventFilterBuilder();
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+
+    public FilterForGreater<Instant> getStartTimestampFrom() {
+        return super.getFrom();
+    }
+
+    public void setStartTimestampFrom(FilterForGreater<Instant> startTimestampFrom) {
+        super.setFrom(startTimestampFrom);
+    }
+
+
+    public FilterForLess<Instant> getStartTimestampTo() {
+        return super.getTo();
+    }
+
+    public void setStartTimestampTo(FilterForLess<Instant> startTimestampTo) {
+        super.setTo(startTimestampTo);
+    }
+
+
+    public StoredTestEventId getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(StoredTestEventId parentId) {
+        this.parentId = parentId;
+        this.root = false;
+    }
+
+    public void setId(StoredTestEventId id) {
+    	this.id = id;
 	}
-	
-	public void setStartTimestampFrom(FilterForGreater<Instant> startTimestampFrom)
-	{
-		super.setFrom(startTimestampFrom);
+
+	public StoredTestEventId getId() {
+    	return id;
 	}
-	
-	
-	public FilterForLess<Instant> getStartTimestampTo()
-	{
-		return super.getTo();
-	}
-	
-	public void setStartTimestampTo(FilterForLess<Instant> startTimestampTo)
-	{
-		super.setTo(startTimestampTo);
-	}
-	
-	
-	public StoredTestEventId getParentId()
-	{
-		return parentId;
-	}
-	
-	public void setParentId(StoredTestEventId parentId)
-	{
-		this.parentId = parentId;
-		this.root = false;
-	}
-	
-	
-	public boolean isRoot()
-	{
-		return root;
-	}
-	
-	public void setRoot()
-	{
-		this.root = true;
-		this.parentId = null;
-	}
-	
-	@Override
-	public String toString()
-	{
-		StringBuilder sb = new StringBuilder("[");
-		if (getBookId() != null)
-			sb.append("bookId=").append(getBookId()).append(TO_STRING_DELIMITER);
-		if (scope != null)
-			sb.append("scope=").append(scope).append(TO_STRING_DELIMITER);
-		if (getFrom() != null)
-			sb.append("timestamp").append(getFrom()).append(TO_STRING_DELIMITER);
-		if (getTo() != null)
-			sb.append("timestamp").append(getTo()).append(TO_STRING_DELIMITER);
-		if (parentId != null)
-			sb.append("parentId=").append(parentId).append(TO_STRING_DELIMITER);
-		if (getLimit() > 0)
-			sb.append("limit=").append(getLimit()).append(TO_STRING_DELIMITER);
-		if (getOrder() != null)
-			sb.append("order=").append(getOrder()).append(TO_STRING_DELIMITER);
-		if (getPageId() != null)
-			sb.append("pageId=").append(getPageId().getName()).append(TO_STRING_DELIMITER);
-		if (sb.length() > 1) //Not only first bracket
-			sb.setLength(sb.length() - TO_STRING_DELIMITER.length());
-		return sb.append("]").toString();
-	}
-	
-	
-	protected void validate() throws CradleStorageException
-	{
-		super.validate();
-		if (StringUtils.isEmpty(scope))
-			throw new CradleStorageException("scope is mandatory");
-	}
+
+
+    public boolean isRoot() {
+        return root;
+    }
+
+    public void setRoot() {
+        this.root = true;
+        this.parentId = null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        if (getBookId() != null)
+            sb.append("bookId=").append(getBookId()).append(TO_STRING_DELIMITER);
+        if (scope != null)
+            sb.append("scope=").append(scope).append(TO_STRING_DELIMITER);
+        if (getFrom() != null)
+            sb.append("timestamp").append(getFrom()).append(TO_STRING_DELIMITER);
+        if (getTo() != null)
+            sb.append("timestamp").append(getTo()).append(TO_STRING_DELIMITER);
+		if (id != null)
+			sb.append("id=").append(id).append(TO_STRING_DELIMITER);
+        if (parentId != null)
+            sb.append("parentId=").append(parentId).append(TO_STRING_DELIMITER);
+        if (getLimit() > 0)
+            sb.append("limit=").append(getLimit()).append(TO_STRING_DELIMITER);
+        if (getOrder() != null)
+            sb.append("order=").append(getOrder()).append(TO_STRING_DELIMITER);
+        if (getPageId() != null)
+            sb.append("pageId=").append(getPageId().getName()).append(TO_STRING_DELIMITER);
+        if (sb.length() > 1) //Not only first bracket
+            sb.setLength(sb.length() - TO_STRING_DELIMITER.length());
+        return sb.append("]").toString();
+    }
+
+
+    protected void validate() throws CradleStorageException {
+        super.validate();
+        if (StringUtils.isEmpty(scope))
+            throw new CradleStorageException("scope is mandatory");
+
+        if (id != null) {
+            if (getOrder() == Order.DIRECT && getFrom() != null)
+                throw new CradleStorageException("from & id filters can not be combined with direct ordering");
+            if (getOrder() == Order.REVERSE && getTo() != null)
+                throw new CradleStorageException("to & id filters can not be combined with reverse ordering");
+        }
+    }
 }
