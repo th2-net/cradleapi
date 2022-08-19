7d9a2424beeff3ef80b38361dbe1dd8eee0c0237
Luka Tchumburidze <lukatchumburidze@gmail.com>
2022-08-19 14:18:36 +0400

    [TH2-3827] bugfix

--- cradle-core/src/main/java/com/exactpro/cradle/testevents/StoredTestEventMetadata.java
+++ cradle-core/src/main/java/com/exactpro/cradle/testevents/StoredTestEventMetadata.java
@@ -20,9 +20,13 @@ import java.io.IOException;
 import java.time.Instant;
 
 import com.exactpro.cradle.utils.TestEventUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class StoredTestEventMetadata implements StoredTestEvent
 {
+	private final Logger logger = LoggerFactory.getLogger(StoredTestEventMetadata.class);
+
 	private StoredTestEventId id;
 	private String name,
 			type;
@@ -198,10 +202,15 @@ public class StoredTestEventMetadata implements StoredTestEvent
 
 	public Instant getMaxStartTimestamp () {
 		Instant maxStart = getStartTimestamp();
-		for (BatchedStoredTestEventMetadata event : batchMetadata.getTestEvents()) {
-			if (maxStart == null || maxStart.isBefore(event.getStartTimestamp())) {
-				maxStart = event.getStartTimestamp();
+
+		try {
+			for (BatchedStoredTestEventMetadata event : getBatchMetadata().getTestEvents()) {
+				if (maxStart == null || maxStart.isBefore(event.getStartTimestamp())) {
+					maxStart = event.getStartTimestamp();
+				}
 			}
+		} catch (IOException e) {
+			logger.warn("Could not get max timestamp from batch metadata: " + e.getMessage());
 		}
 
 		return maxStart;
