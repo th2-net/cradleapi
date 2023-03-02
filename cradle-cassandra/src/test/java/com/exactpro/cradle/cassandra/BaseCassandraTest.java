10c680286901ddc415ef8f09ee9758bc32452a1e
Luka Tchumburidze <lukatchumburidze@gmail.com>
2023-03-02 13:30:58 +0400

    [TH2-4731] added book&pages init in BaseCassandraTest

--- cradle-cassandra/src/test/java/com/exactpro/cradle/cassandra/BaseCassandraTest.java
+++ cradle-cassandra/src/test/java/com/exactpro/cradle/cassandra/BaseCassandraTest.java
@@ -16,11 +16,93 @@
 
 package com.exactpro.cradle.cassandra;
 
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.exactpro.cradle.*;
 import com.exactpro.cradle.cassandra.keyspaces.TableTruncator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BaseCassandraTest {
 
-    protected void startUp() {
+    private static final Logger logger = LoggerFactory.getLogger(BaseCassandraTest.class);
+
+
+    protected static final String DEFAULT_PAGE_PREFIX = "test_page_";
+    protected static final BookId DEFAULT_BOOK_ID = new BookId("test_book");
+    private static final Instant DEFAULT_DATA_END = Instant.now();
+    private static final Instant DEFAULT_DATA_START = DEFAULT_DATA_END.minus(1, ChronoUnit.HOURS);
+
+    private static final List<PageInfo> DEFAULT_PAGES = List.of(
+            new PageInfo(
+                    new PageId(DEFAULT_BOOK_ID, DEFAULT_PAGE_PREFIX+0),
+                    DEFAULT_DATA_START,
+                    DEFAULT_DATA_START.plus(10, ChronoUnit.MINUTES), ""),
+            new PageInfo(
+                    new PageId(DEFAULT_BOOK_ID, DEFAULT_PAGE_PREFIX+1),
+                    DEFAULT_DATA_START.plus(10, ChronoUnit.MINUTES),
+                    DEFAULT_DATA_START.plus(20, ChronoUnit.MINUTES), ""),
+            new PageInfo(
+                    new PageId(DEFAULT_BOOK_ID, DEFAULT_PAGE_PREFIX+2),
+                    DEFAULT_DATA_START.plus(20, ChronoUnit.MINUTES),
+                    DEFAULT_DATA_START.plus(30, ChronoUnit.MINUTES), ""),
+            new PageInfo(
+                    new PageId(DEFAULT_BOOK_ID, DEFAULT_PAGE_PREFIX+3),
+                    DEFAULT_DATA_START.plus(30, ChronoUnit.MINUTES),
+                    DEFAULT_DATA_START.plus(40, ChronoUnit.MINUTES), ""),
+            new PageInfo(
+                    new PageId(DEFAULT_BOOK_ID, DEFAULT_PAGE_PREFIX+4),
+                    DEFAULT_DATA_START.plus(40, ChronoUnit.MINUTES),
+                    DEFAULT_DATA_START.plus(50, ChronoUnit.MINUTES), ""),
+            new PageInfo(
+                    new PageId(DEFAULT_BOOK_ID, DEFAULT_PAGE_PREFIX+5),
+                    DEFAULT_DATA_START.plus(50, ChronoUnit.MINUTES),
+                    DEFAULT_DATA_START.plus(60, ChronoUnit.MINUTES), ""));
+
+
+    protected List<PageInfo> pages = DEFAULT_PAGES;
+    protected CqlSession session;
+    protected CassandraCradleStorage storage;
+    protected Instant dataStart = DEFAULT_DATA_START;
+    protected Instant dataEnd = DEFAULT_DATA_END;
+    protected BookId bookId = DEFAULT_BOOK_ID;
+
+    protected void startUp () {
+        startUp(false);
+    }
+
+    protected void startUp(boolean initBooksAndPages) {
         TableTruncator.truncateTables(CassandraCradleHelper.getInstance().getSession(), CassandraCradleHelper.KEYSPACE_NAME);
+
+
+        this.session = CassandraCradleHelper.getInstance().getSession();
+        this.storage = CassandraCradleHelper.getInstance().getStorage();
+
+        if (initBooksAndPages) {
+            setUpBooksAndPages(
+                    DEFAULT_BOOK_ID,
+                    DEFAULT_PAGES.stream().map(
+                            el -> new PageToAdd(
+                                    el.getId().getName(),
+                                    el.getStarted(),
+                                    el.getComment())).collect(Collectors.toList()));
+        }
+    }
+
+    protected void setUpBooksAndPages (BookId bookId, List<PageToAdd> pagesToAdd) {
+        try {
+            storage.addBook(new BookToAdd(bookId.getName(), dataStart));
+
+            BookInfo book = storage.addPages(bookId, pagesToAdd);
+
+            pages = new ArrayList<>(book.getPages());
+        } catch (Exception e) {
+            logger.error("", e);
+        }
     }
 }
--- cradle-cassandra/src/test/java/com/exactpro/cradle/cassandra/dao/messages/GroupedMessageIteratorProviderTest.java
+++ cradle-cassandra/src/test/java/com/exactpro/cradle/cassandra/dao/messages/GroupedMessageIteratorProviderTest.java
@@ -16,11 +16,9 @@
 
 package com.exactpro.cradle.cassandra.dao.messages;
 
-import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.MappedAsyncPagingIterable;
 import com.exactpro.cradle.*;
 import com.exactpro.cradle.cassandra.BaseCassandraTest;
-import com.exactpro.cradle.cassandra.CassandraCradleStorage;
 import com.exactpro.cradle.cassandra.CassandraCradleHelper;
 import com.exactpro.cradle.cassandra.dao.CassandraDataMapper;
 import com.exactpro.cradle.cassandra.dao.CassandraDataMapperBuilder;
@@ -37,7 +35,6 @@ import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Collection;
 import java.util.List;
@@ -52,69 +49,19 @@ public class GroupedMessageIteratorProviderTest extends BaseCassandraTest {
 
 
     public static String content = "default_content";
-    private static final BookId BOOKID = new BookId("test_book");
-    private static final String PAGE_PREFIX = "test_page_";
     private static final String GROUP_NAME = "test_group";
     private static final int DEFAULT_LIMIT = 10;
-
-    private static final Instant END = Instant.now();
-    private static final Instant START = END.minus(1, ChronoUnit.HOURS);
 
     private List<GroupedMessageBatchToStore> data;
     private CompletableFuture<MappedAsyncPagingIterable<GroupedMessageBatchEntity>> iterable;
     private CassandraOperators operators;
     private ExecutorService composingService = Executors.newSingleThreadExecutor();
     public final static String protocol = "default_message_protocol";
-    private CqlSession session;
-    private CassandraCradleStorage storage;
-    private List<PageInfo> pages;
-
     @BeforeClass
     public void startUp () {
-        super.startUp();
-        this.session = CassandraCradleHelper.getInstance().getSession();
-        this.storage = CassandraCradleHelper.getInstance().getStorage();
+        super.startUp(true);
 
         setUpOperators ();
-        setUpBooksAndPages();
-    }
-
-    private void setUpBooksAndPages () {
-        try {
-            storage.addBook(new BookToAdd(BOOKID.getName(), START));
-
-            pages = List.of(
-                    new PageInfo(
-                            new PageId(BOOKID, PAGE_PREFIX+0),
-                            START,
-                            START.plus(10, ChronoUnit.MINUTES), ""),
-                    new PageInfo(
-                            new PageId(BOOKID, PAGE_PREFIX+1),
-                            START.plus(10, ChronoUnit.MINUTES),
-                            START.plus(20, ChronoUnit.MINUTES), ""),
-                    new PageInfo(
-                            new PageId(BOOKID, PAGE_PREFIX+2),
-                            START.plus(20, ChronoUnit.MINUTES),
-                            START.plus(30, ChronoUnit.MINUTES), ""),
-                    new PageInfo(
-                            new PageId(BOOKID, PAGE_PREFIX+3),
-                            START.plus(30, ChronoUnit.MINUTES),
-                            START.plus(40, ChronoUnit.MINUTES), ""),
-                    new PageInfo(
-                            new PageId(BOOKID, PAGE_PREFIX+4),
-                            START.plus(40, ChronoUnit.MINUTES),
-                            START.plus(50, ChronoUnit.MINUTES), ""),
-                    new PageInfo(
-                            new PageId(BOOKID, PAGE_PREFIX+5),
-                            START.plus(50, ChronoUnit.MINUTES),
-                            START.plus(60, ChronoUnit.MINUTES), ""));
-
-            for (var el : pages) {
-                storage.addPage(BOOKID, el.getId().getName(), el.getStarted(), el.getComment());
-            }
-        } catch (CradleStorageException | IOException e) {
-            logger.info("", e);
-        }
     }
 
     @Test
@@ -144,16 +91,16 @@ public class GroupedMessageIteratorProviderTest extends BaseCassandraTest {
             GroupedMessageFilter groupMessageFilter;
 
             if (initPage == null) {
-                groupMessageFilter = new GroupedMessageFilter(BOOKID, groupName);
+                groupMessageFilter = new GroupedMessageFilter(bookId, groupName);
             } else {
-                groupMessageFilter = new GroupedMessageFilter(BOOKID, new PageId(BOOKID, initPage), groupName);
+                groupMessageFilter = new GroupedMessageFilter(bookId, new PageId(bookId, initPage), groupName);
             }
 
             return new GroupedMessageIteratorProvider(
                     "",
                         groupMessageFilter,
                         operators,
-                        storage.refreshBook(BOOKID.getName()),
+                        storage.refreshBook(bookId.getName()),
                         composingService,
                         new SelectQueryExecutor(session, composingService, null, null),
                         storage.getReadAttrs(),
@@ -172,60 +119,60 @@ public class GroupedMessageIteratorProviderTest extends BaseCassandraTest {
     private void generateData () throws CradleStorageException {
         var b1 =  new GroupedMessageBatchToStore(GROUP_NAME, 1024);
         b1.addMessage(MessageToStore.builder()
-                .bookId(BOOKID)
+                .bookId(bookId)
                 .sessionAlias(GROUP_NAME + 1)
                 .direction(Direction.FIRST)
-                .timestamp(START.plus(5, ChronoUnit.MINUTES))
+                .timestamp(dataStart.plus(5, ChronoUnit.MINUTES))
                 .sequence(1L)
                 .content(content.getBytes(StandardCharsets.UTF_8))
                 .protocol(protocol)
                 .build());
 
         b1.addMessage(MessageToStore.builder()
-                .bookId(BOOKID)
+                .bookId(bookId)
                 .sessionAlias(GROUP_NAME + 1)
                 .direction(Direction.SECOND)
-                .timestamp(START.plus(9, ChronoUnit.MINUTES))
+                .timestamp(dataStart.plus(9, ChronoUnit.MINUTES))
                 .sequence(2L)
                 .content(content.getBytes(StandardCharsets.UTF_8))
                 .protocol(protocol)
                 .build());
         var b2 =  new GroupedMessageBatchToStore(GROUP_NAME, 1024);
         b2.addMessage(MessageToStore.builder()
-                .bookId(BOOKID)
+                .bookId(bookId)
                 .sessionAlias(GROUP_NAME + 1)
                 .direction(Direction.FIRST)
-                .timestamp(START.plus(15, ChronoUnit.MINUTES))
+                .timestamp(dataStart.plus(15, ChronoUnit.MINUTES))
                 .sequence(3L)
                 .content(content.getBytes(StandardCharsets.UTF_8))
                 .protocol(protocol)
                 .build());
 
         b2.addMessage(MessageToStore.builder()
-                .bookId(BOOKID)
+                .bookId(bookId)
                 .sessionAlias(GROUP_NAME + 1)
                 .direction(Direction.SECOND)
-                .timestamp(START.plus(19, ChronoUnit.MINUTES))
+                .timestamp(dataStart.plus(19, ChronoUnit.MINUTES))
                 .sequence(4L)
                 .content(content.getBytes(StandardCharsets.UTF_8))
                 .protocol(protocol)
                 .build());
         var b3 =  new GroupedMessageBatchToStore(GROUP_NAME, 1024);
         b3.addMessage(MessageToStore.builder()
-                .bookId(BOOKID)
+                .bookId(bookId)
                 .sessionAlias(GROUP_NAME + 1)
                 .direction(Direction.FIRST)
-                .timestamp(START.plus(25, ChronoUnit.MINUTES))
+                .timestamp(dataStart.plus(25, ChronoUnit.MINUTES))
                 .sequence(5L)
                 .content(content.getBytes(StandardCharsets.UTF_8))
                 .protocol(protocol)
                 .build());
 
         b3.addMessage(MessageToStore.builder()
-                .bookId(BOOKID)
+                .bookId(bookId)
                 .sessionAlias(GROUP_NAME + 1)
                 .direction(Direction.SECOND)
-                .timestamp(START.plus(29, ChronoUnit.MINUTES))
+                .timestamp(dataStart.plus(29, ChronoUnit.MINUTES))
                 .sequence(6L)
                 .content(content.getBytes(StandardCharsets.UTF_8))
                 .protocol(protocol)
