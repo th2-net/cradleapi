a60b3299a6ed7a8f3cec2304fbaa7853ff8e98d8
nikita.smirnov <nikita.smirnov@exactprosystems.com>
2023-06-06 20:44:43 +0400

    Reverted backward compatible

--- cradle-core/src/main/java/com/exactpro/cradle/testevents/TestEventToStore.java
+++ cradle-core/src/main/java/com/exactpro/cradle/testevents/TestEventToStore.java
@@ -1,17 +1,17 @@
 /*
- * Copyright 2020-2023 Exactpro (Exactpro Systems Limited)
+ *  Copyright 2023 Exactpro (Exactpro Systems Limited)
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package com.exactpro.cradle.testevents;
@@ -20,6 +20,8 @@ import com.exactpro.cradle.utils.CradleStorageException;
 import com.exactpro.cradle.utils.TestEventUtils;
 
 import java.time.Instant;
+
+import static com.exactpro.cradle.CoreStorageSettings.DEFAULT_BOOK_REFRESH_INTERVAL_MILLIS;
 
 /**
  * Holds basic information about test event prepared to be stored in Cradle. Events extend this class with additional data
@@ -37,6 +39,22 @@ public abstract class TestEventToStore implements TestEvent {
         this.name = name;
         this.parentId = parentId;
         TestEventUtils.validateTestEvent(this, storeActionRejectionThreshold);
+    }
+
+    public static TestEventSingleToStoreBuilder singleBuilder(long storeActionRejectionThreshold) {
+        return new TestEventSingleToStoreBuilder(storeActionRejectionThreshold);
+    }
+
+    public static TestEventSingleToStoreBuilder singleBuilder() {
+        return singleBuilder(DEFAULT_BOOK_REFRESH_INTERVAL_MILLIS);
+    }
+
+    public static TestEventBatchToStoreBuilder batchBuilder(int maxBatchSize, long storeActionRejectionThreshold) {
+        return new TestEventBatchToStoreBuilder(maxBatchSize, storeActionRejectionThreshold);
+    }
+
+    public static TestEventBatchToStoreBuilder batchBuilder(int maxBatchSize) {
+        return batchBuilder(maxBatchSize, DEFAULT_BOOK_REFRESH_INTERVAL_MILLIS);
     }
 
     @Override
