9191fd21e2da99f21073d409571196e469a0a7d3
nikita.smirnov <nikita.smirnov@exactprosystems.com>
2024-02-20 17:42:10 +0400

    [TH2-5165] Added Iterator<PageInfo> getPages

--- cradle-core/src/main/java/com/exactpro/cradle/BookInfo.java
+++ cradle-core/src/main/java/com/exactpro/cradle/BookInfo.java
@@ -21,16 +21,20 @@ import com.github.benmanes.caffeine.cache.LoadingCache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.NavigableMap;
 import java.util.TreeMap;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 import static com.exactpro.cradle.BookInfoMetrics.CacheName.HOT;
 import static com.exactpro.cradle.BookInfoMetrics.CacheName.RANDOM;
@@ -38,6 +42,7 @@ import static com.exactpro.cradle.BookInfoMetrics.RequestMethod.FIND;
 import static com.exactpro.cradle.BookInfoMetrics.RequestMethod.GET;
 import static com.exactpro.cradle.BookInfoMetrics.RequestMethod.NEXT;
 import static com.exactpro.cradle.BookInfoMetrics.RequestMethod.PREVIOUS;
+import static java.util.Collections.emptyIterator;
 import static java.util.Collections.unmodifiableMap;
 import static java.util.Collections.unmodifiableNavigableMap;
 
@@ -160,12 +165,99 @@ public class BookInfo
 		METRICS.incRequest(id.getName(), pageInterval.getCacheName(), GET);
 		return pageInterval.get(pageId);
 	}
-	
-	public PageInfo findPage(Instant timestamp)
-	{
-		IPageInterval pageInterval = getPageInterval(timestamp);
+
+	/**
+	 * Requests page iterator using cache. Iterator lazy loads pages into cache.
+	 */
+	public Iterator<PageInfo> getPages(@Nullable Instant leftBoundTimestamp,
+									   @Nullable Instant rightBoundTimestamp,
+									   @Nonnull Order order) {
+		Instant leftTimestamp = calculateBound(leftBoundTimestamp, this::getFirstPage);
+		if (leftTimestamp == null) {
+			return emptyIterator();
+		}
+		Instant rightTimestamp = calculateBound(rightBoundTimestamp, this::getLastPage);
+		if (rightTimestamp == null) {
+			return emptyIterator();
+		}
+
+		if (leftTimestamp.isAfter(rightTimestamp)) {
+			throw new IllegalArgumentException(
+					"Left bound '" + leftBoundTimestamp + " -> " + leftTimestamp +
+							"' must be <= right bound '" + rightBoundTimestamp + " -> " + rightTimestamp + "'"
+			);
+		}
+		long leftBoundEpochDay = getEpochDay(leftTimestamp);
+		long rightBoundEpochDay = getEpochDay(rightTimestamp);
+
+		Stream.Builder<Long> builder = Stream.builder();
+		switch (order) {
+            case DIRECT:
+				for (long i = leftBoundEpochDay; i <= rightBoundEpochDay; i++) {
+					builder.add(i);
+				}
+				break;
+            case REVERSE:
+				for (long i = rightBoundEpochDay; i >= leftBoundEpochDay; i--) {
+					builder.add(i);
+				}
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + order);
+        }
+		return builder.build()
+				.flatMap(epochDay -> getPageInterval(epochDay).getPages(leftTimestamp, rightTimestamp, order))
+				.iterator();
+	}
+
+	public PageInfo findPage(Instant timestamp) {
+		// Search in the page interval related to the timestamp
+		long epochDay = getEpochDay(timestamp);
+		IPageInterval pageInterval = getPageInterval(epochDay);
+		PageInfo pageInfo = pageInterval.find(timestamp);
 		METRICS.incRequest(id.getName(), pageInterval.getCacheName(), FIND);
-		return pageInterval.find(timestamp);
+		if (pageInfo != null) {
+			return pageInfo;
+		}
+
+		// Search in page intervals before the timestamp
+		PageInfo firstPage = getFirstPage();
+		if (firstPage == null) {
+			return null;
+		}
+		if (firstPage.getStarted().isAfter(timestamp)) {
+			return null;
+		}
+
+		long firstEpochDay = getEpochDay(firstPage.getStarted());
+		if (epochDay < firstEpochDay) {
+			return null;
+		} else if (epochDay > firstEpochDay) {
+			for (long previousEpochDay = epochDay - 1; previousEpochDay >= firstEpochDay; previousEpochDay--) {
+				IPageInterval previousPageInterval = getPageInterval(previousEpochDay);
+				if (previousPageInterval.size() == 0) {
+					// page interval without new pages
+					continue;
+				}
+				PageInfo previousPageInfo = previousPageInterval.find(timestamp);
+				METRICS.incRequest(id.getName(), previousPageInterval.getCacheName(), FIND);
+				if (previousPageInfo == null) {
+					throw new IllegalStateException(
+							"Previous page interval isn't empty but doesn't contain page covers date " + timestamp
+					);
+				}
+				return previousPageInfo;
+			}
+			throw new IllegalStateException(
+					"First page is before requested timestamp, but neither cache doesn't contain appropriate page, requested: " +
+							timestamp + ", first page: " + firstPage.getStarted()
+			);
+		} else {
+			throw new IllegalStateException(
+					"First page is in the same day as requested timestamp, but cache couldn't find appropriate page, requested: " +
+							timestamp + ", first page: " + firstPage.getStarted()
+			);
+		}
 	}
 
 	public PageInfo getNextPage(Instant startTimestamp)
@@ -181,7 +273,7 @@ public class BookInfo
 		METRICS.incRequest(id.getName(), pageInterval.getCacheName(), NEXT);
 		return pageInterval.next(startTimestamp);
 	}
-	
+
 	public PageInfo getPreviousPage(Instant startTimestamp)
 	{
 		long epochDate = getEpochDay(startTimestamp);
@@ -214,12 +306,19 @@ public class BookInfo
 				.forEach(this::invalidate);
 	}
 
-	void removePage(PageId pageId) {
-		invalidate(pageId.getStart());
-	}
-	
-	void addPage(PageInfo page) {
-		invalidate(page.getId().getStart());
+	private @Nullable Instant calculateBound(@Nullable Instant origin, Supplier<PageInfo> supplier) {
+		if (origin != null) {
+			PageInfo pageInfo = findPage(origin);
+			if (pageInfo != null) {
+				return pageInfo.getStarted();
+			}
+			return null;
+		}
+		PageInfo pageInfo = supplier.get();
+		if (pageInfo == null) {
+			return null;
+		}
+		return pageInfo.getStarted();
 	}
 
 	private void invalidate(long epochDay) {
@@ -228,8 +327,8 @@ public class BookInfo
 	}
 
 	private IPageInterval getPageInterval(long epochDate) {
-		long currentEpochDate = currentEpochDay();
-		IPageInterval pageInterval = currentEpochDate - epochDate < 2
+		long currentEpochDay = currentEpochDay();
+		IPageInterval pageInterval = currentEpochDay - epochDate < 2
 				? hotCache.get(epochDate)
 				: randomAccessCache.get(epochDate);
 		return pageInterval != null
@@ -271,6 +370,8 @@ public class BookInfo
 		PageInfo next(Instant startTimestamp);
 
 		PageInfo previous(Instant startTimestamp);
+
+		Stream<PageInfo> getPages(Instant leftBoundTimestamp, Instant rightBoundTimestamp, Order order);
 	}
 
 	private static final class EmptyPageInterval implements IPageInterval {
@@ -302,6 +403,11 @@ public class BookInfo
 		@Override
 		public PageInfo previous(Instant startTimestamp) {
 			return null;
+		}
+
+		@Override
+		public Stream<PageInfo> getPages(Instant leftBoundTimestamp, Instant rightBoundTimestamp, Order order) {
+			return Stream.empty();
 		}
 	}
 
@@ -347,6 +453,25 @@ public class BookInfo
 		public PageInfo previous(Instant startTimestamp) {
 			Entry<Instant, PageInfo> result = pageByInstant.lowerEntry(startTimestamp);
 			return result != null ? result.getValue() : null;
+		}
+
+		@Override
+		public Stream<PageInfo> getPages(@Nonnull Instant leftBoundTimestamp,
+										   @Nonnull Instant rightBoundTimestamp,
+										   @Nonnull Order order) {
+			Instant start = pageByInstant.floorKey(leftBoundTimestamp);
+			NavigableMap<Instant, PageInfo> subMap = pageByInstant.subMap(
+					start == null ? leftBoundTimestamp : start, true,
+					rightBoundTimestamp, true
+			);
+			switch (order) {
+                case DIRECT:
+					return subMap.values().stream();
+                case REVERSE:
+					return subMap.descendingMap().values().stream();
+				default:
+					throw new IllegalArgumentException("Unsupported order: " + order);
+            }
 		}
 	}
 
--- cradle-core/src/test/java/com/exactpro/cradle/BookInfoTest.java
+++ cradle-core/src/test/java/com/exactpro/cradle/BookInfoTest.java
@@ -15,6 +15,8 @@
  */
 package com.exactpro.cradle;
 
+import com.google.common.collect.Lists;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import javax.annotation.Nullable;
@@ -22,9 +24,13 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
+import static java.time.temporal.ChronoUnit.DAYS;
+import static java.time.temporal.ChronoUnit.NANOS;
+import static java.util.Collections.emptyList;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNull;
 import static org.testng.Assert.assertSame;
@@ -45,7 +51,7 @@ public class BookInfoTest {
             current = current.plus(1, ChronoUnit.HOURS);
             pages.add(createPageInfo(previous, current));
         } while (current.isBefore(end));
-        pages.add(createPageInfo(current, current));
+        pages.add(createPageInfo(current, null));
 
         PAGES = Collections.unmodifiableList(pages);
     }
@@ -57,35 +63,99 @@ public class BookInfoTest {
 
         assertNull(bookInfo.getFirstPage());
         assertNull(bookInfo.getLastPage());
-        assertEquals(bookInfo.getPages(), Collections.emptyList());
+        assertEquals(bookInfo.getPages(), emptyList());
 
-        for (int i = 0; i < PAGES.size(); i++) {
-            PageInfo newPage = PAGES.get(i);
+        for (int addIndex = 0; addIndex < PAGES.size(); addIndex++) {
+            PageInfo newPage = PAGES.get(addIndex);
             operateSource.add(newPage);
             bookInfo.invalidate();
 
-            assertSame(bookInfo.getFirstPage(), PAGES.get(0), "iteration - " + i);
-            assertSame(bookInfo.getLastPage(), newPage, "iteration - " + i);
-            assertEquals(bookInfo.getPages(), PAGES.subList(0, i + 1), "iteration - " + i);
+            assertSame(bookInfo.getFirstPage(), PAGES.get(0), "iteration - " + addIndex);
+            assertSame(bookInfo.getLastPage(), newPage, "iteration - " + addIndex);
+            assertEquals(bookInfo.getPages(), PAGES.subList(0, addIndex + 1), "iteration - " + addIndex);
 
-            for (int j = i; j >= 0; j--) {
-                PageInfo source = PAGES.get(j);
-                assertSame(bookInfo.getPage(source.getId()), source, "iteration - " + i + '.' + j);
-                assertSame(bookInfo.findPage(source.getId().getStart()), source, "iteration - " + i + '.' + j);
+            for (int checkIndex = addIndex; checkIndex >= 0; checkIndex--) {
+                PageInfo source = PAGES.get(checkIndex);
+                assertSame(bookInfo.getPage(source.getId()), source, "iteration - " + addIndex + '.' + checkIndex);
+                assertSame(bookInfo.findPage(source.getId().getStart()), source, "iteration - " + addIndex + '.' + checkIndex);
 
-                if (j > 0) {
-                    assertSame(bookInfo.getPreviousPage(source.getId().getStart()), PAGES.get(j - 1), "iteration - " + i + '.' + j);
+                if (checkIndex > 0) {
+                    assertSame(bookInfo.getPreviousPage(source.getId().getStart()), PAGES.get(checkIndex - 1), "iteration - " + addIndex + '.' + checkIndex);
                 } else {
-                    assertNull(bookInfo.getPreviousPage(source.getId().getStart()), "iteration - " + i + '.' + j + ", timestamp: " + source.getId().getStart());
+                    assertNull(bookInfo.getPreviousPage(source.getId().getStart()), "iteration - " + addIndex + '.' + checkIndex + ", timestamp: " + source.getId().getStart());
                 }
-                if (j < i) {
-                    assertSame(bookInfo.getNextPage(source.getId().getStart()), PAGES.get(j + 1), "iteration - " + i + '.' + j);
+                if (checkIndex < addIndex) {
+                    assertSame(bookInfo.getNextPage(source.getId().getStart()), PAGES.get(checkIndex + 1), "iteration - " + addIndex + '.' + checkIndex);
                 } else {
-                    assertNull(bookInfo.getNextPage(source.getId().getStart()), "iteration - " + i + '.' + j + ", timestamp: " + source.getId().getStart());
+                    assertNull(bookInfo.getNextPage(source.getId().getStart()), "iteration - " + addIndex + '.' + checkIndex + ", timestamp: " + source.getId().getStart());
                 }
             }
         }
     }
+
+    @Test
+    public void getAllPagesInDirectOrderTest() {
+        List<PageInfo> operateSource = new ArrayList<>(PAGES);
+        BookInfo bookInfo = createBookInfo(operateSource);
+
+        Iterator<PageInfo> iterator = bookInfo.getPages(null, null, Order.DIRECT);
+        assertEquals(Lists.newArrayList(iterator), operateSource);
+    }
+
+    @Test(dataProvider = "orders")
+    // ... ps   |      |   ps  ps ...
+    public void getPagesByPointTest(Order order) {
+        Instant time1 = Instant.parse("2024-02-13T12:00:00Z");
+        Instant time2 = Instant.parse("2024-02-15T12:00:00Z");
+        Instant time3 = Instant.parse("2024-02-15T18:00:00Z");
+        List<PageInfo> operateSource = List.of(
+                createPageInfo(time1, time2),
+                createPageInfo(time2, time3),
+                createPageInfo(time3, null)
+        );
+        BookInfo bookInfo = createBookInfo(operateSource);
+
+        assertEquals(
+                Lists.newArrayList(bookInfo.getPages(time1.minus(1, NANOS), time1.minus(1, NANOS), order)),
+                emptyList(),
+                "Point before first page start"
+        );
+        assertEquals(
+                Lists.newArrayList(bookInfo.getPages(time1, time1, order)),
+                operateSource.subList(0, 1),
+                "Point equals first page start"
+        );
+        assertEquals(
+                Lists.newArrayList(bookInfo.getPages(time1.plus(1, NANOS), time1.plus(1, NANOS), order)),
+                operateSource.subList(0, 1),
+                "Point after first page start"
+        );
+        assertEquals(
+                Lists.newArrayList(bookInfo.getPages(time1.plus(1, DAYS), time1.plus(1, DAYS), order)),
+                operateSource.subList(0, 1),
+                "Point in the middle of first page"
+        );
+        assertEquals(
+                Lists.newArrayList(bookInfo.getPages(time2.minus(1, NANOS), time2.minus(1, NANOS), order)),
+                operateSource.subList(0, 1),
+                "Point before first page end"
+        );
+        assertEquals(
+                Lists.newArrayList(bookInfo.getPages(time2, time2, order)),
+                operateSource.subList(1, 2),
+                "Point equals second page start"
+        );
+    }
+
+    @Test
+    public void getAllPagesInReverseOrderTest() {
+        List<PageInfo> source = new ArrayList<>(PAGES);
+        BookInfo bookInfo = createBookInfo(source);
+
+        Iterator<PageInfo> iterator = bookInfo.getPages(null, null, Order.REVERSE);
+        assertEquals(Lists.newArrayList(iterator), Lists.reverse(source));
+    }
+
 
     @Test
     public void removePageTest() {
@@ -104,7 +174,7 @@ public class BookInfoTest {
             assertEquals(bookInfo.getPages(), operateSource, "iteration - " + iteration);
 
             operateSource.remove(index);
-            bookInfo.removePage(pageForRemove.getId());
+            bookInfo.invalidate(pageForRemove.getStarted());
 
             assertNull(bookInfo.getPage(pageForRemove.getId()), "iteration - " + iteration);
 
@@ -126,7 +196,7 @@ public class BookInfoTest {
         for (int i = 0; i < PAGES.size(); i++) {
             PageInfo page = PAGES.get(i);
             pages.add(page);
-            bookInfo.addPage(page);
+            bookInfo.invalidate(page.getStarted());
 
             assertSame(bookInfo.getFirstPage(), pages.get(0), "iteration - " + i);
             assertSame(bookInfo.getLastPage(), pages.get(pages.size() - 1), "iteration - " + i);
@@ -150,5 +220,10 @@ public class BookInfoTest {
 
     private static PageInfo createPageInfo(Instant start, @Nullable Instant end) {
         return new PageInfo(new PageId(BOOK_ID, start, start.toString()), end, "test-comment");
+    }
+
+    @DataProvider(name = "orders")
+    public Order[] orders() {
+        return Order.values();
     }
 }
