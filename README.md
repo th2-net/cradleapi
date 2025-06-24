a50a5cd3ee3701c4c382528de5e8120b2e5fe93f
Nikita Smirnov <46124551+Nikita-Smirnov-Exactpro@users.noreply.github.com>
2025-06-24 14:56:06 +0400

    [GH-308] Provided ability to update parent event status before storing child event (#309)
    * Added updateStatusBeforeStoringEvent core settings
    * Added logging for failEventAndParents method

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Overview (5.5.1)
+# Overview (5.6.0)
 
 Cradle API is used to work with Cradle - the datalake where th2 stores its data.
 
@@ -208,6 +208,12 @@ Test events have mandatory parameters that are verified when storing an event. T
   * cause: EXPLICIT, REPLACED, COLLECTED, EXPIRED, SIZE
 
 ## Release notes
+
+### 5.6.0
+* Provided ability to update parent event status before storing child event.
+  Added `updateStatusBeforeStoringEvent` option into core cradle settings
+* Updated:
+  * caffeine: `3.2.1`
 
 ### 5.5.1
 * Fixed the problem - grouped messages queries with limit on number of batches to return could return no batches when there is a batch that falls under condition.
--- cradle-cassandra/src/test/java/com/exactpro/cradle/cassandra/CassandraStorageSettingsTest.java
+++ cradle-cassandra/src/test/java/com/exactpro/cradle/cassandra/CassandraStorageSettingsTest.java
@@ -16,18 +16,69 @@
 
 package com.exactpro.cradle.cassandra;
 
+import com.datastax.oss.driver.api.core.cql.Statement;
+import com.exactpro.cradle.cassandra.connection.NetworkTopologyStrategy;
+import com.exactpro.cradle.cassandra.retries.SelectExecutionPolicy;
+import com.exactpro.cradle.cassandra.retries.SelectExecutionVerdict;
+import com.exactpro.cradle.utils.CompressionType;
 import org.assertj.core.api.Assertions;
 import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 public class CassandraStorageSettingsTest {
     @Test
     public void copy() {
-        CassandraStorageSettings settings = new CassandraStorageSettings();
-        settings.setMaxUncompressedTestEventSize(settings.getMaxUncompressedTestEventSize() + 10);
+        CassandraStorageSettings defaultSettings = new CassandraStorageSettings();
+        defaultSettings.setBookRefreshIntervalMillis(defaultSettings.getBookRefreshIntervalMillis() + 10);
+        defaultSettings.setUpdateStatusBeforeStoringEvent(!defaultSettings.isUpdateStatusBeforeStoringEvent());
+        defaultSettings.setMaxUncompressedTestEventSize(defaultSettings.getMaxUncompressedTestEventSize() + 10);
 
-        CassandraStorageSettings copy = new CassandraStorageSettings(settings);
+        defaultSettings.setNetworkTopologyStrategy(new NetworkTopologyStrategy(Collections.singletonMap("test", 123)));
+        defaultSettings.setTimeout(defaultSettings.getTimeout() + 10);
+        defaultSettings.setWriteConsistencyLevel(Arrays.stream(CassandraConsistencyLevel.values()).filter(item -> item != defaultSettings.getWriteConsistencyLevel()).findFirst().orElseThrow());
+        defaultSettings.setReadConsistencyLevel(Arrays.stream(CassandraConsistencyLevel.values()).filter(item -> item != defaultSettings.getReadConsistencyLevel()).findFirst().orElseThrow());
+        defaultSettings.setKeyspace(defaultSettings.getKeyspace() + 10);
+        defaultSettings.setKeyspaceReplicationFactor(defaultSettings.getKeyspaceReplicationFactor() + 10);
+        defaultSettings.setMaxParallelQueries(defaultSettings.getMaxParallelQueries() + 10);
+        defaultSettings.setResultPageSize(defaultSettings.getResultPageSize() + 10);
+        defaultSettings.setMaxMessageBatchSize(defaultSettings.getMaxMessageBatchSize() + 10);
+        defaultSettings.setMaxUncompressedMessageBatchSize(defaultSettings.getMaxUncompressedMessageBatchSize() + 10);
+        defaultSettings.setMaxTestEventBatchSize(defaultSettings.getMaxTestEventBatchSize() + 10);
+        defaultSettings.setMaxUncompressedTestEventSize(defaultSettings.getMaxUncompressedTestEventSize() + 10);
+        defaultSettings.setSessionsCacheSize(defaultSettings.getSessionsCacheSize() + 10);
+        defaultSettings.setPageSessionsCacheSize(defaultSettings.getPageSessionsCacheSize() + 10);
+        defaultSettings.setSessionStatisticsCacheSize(defaultSettings.getSessionStatisticsCacheSize() + 10);
+        defaultSettings.setScopesCacheSize(defaultSettings.getScopesCacheSize() + 10);
+        defaultSettings.setPageScopesCacheSize(defaultSettings.getPageScopesCacheSize() + 10);
+        defaultSettings.setPageGroupsCacheSize(defaultSettings.getPageGroupsCacheSize() + 10);
+        defaultSettings.setGroupsCacheSize(defaultSettings.getGroupsCacheSize() + 10);
+        defaultSettings.setCounterPersistenceInterval(defaultSettings.getCounterPersistenceInterval() + 10);
+        defaultSettings.setCounterPersistenceMaxParallelQueries(defaultSettings.getCounterPersistenceMaxParallelQueries() + 10);
+        defaultSettings.setComposingServiceThreads(defaultSettings.getComposingServiceThreads() + 10);
+        defaultSettings.setMultiRowResultExecutionPolicy(new TestSelectExecutionPolicy());
+        defaultSettings.setSingleRowResultExecutionPolicy(new TestSelectExecutionPolicy());
+        defaultSettings.setEventBatchDurationMillis(defaultSettings.getEventBatchDurationMillis() + 10);
+        defaultSettings.setEventBatchDurationCacheSize(defaultSettings.getEventBatchDurationCacheSize() + 10);
+        defaultSettings.setCompressionType(Arrays.stream(CompressionType.values()).filter(item -> item != defaultSettings.getCompressionType()).findFirst().orElseThrow());
+        defaultSettings.setInitOperatorsDurationSeconds(defaultSettings.getInitOperatorsDurationSeconds() + 10);
+
+        CassandraStorageSettings copy = new CassandraStorageSettings(defaultSettings);
         Assertions.assertThat(copy)
                 .usingRecursiveComparison()
-                .isEqualTo(settings);
+                .isEqualTo(defaultSettings);
+    }
+
+    private static class TestSelectExecutionPolicy implements SelectExecutionPolicy {
+        @Override
+        public SelectExecutionVerdict onError(Statement<?> statement, String queryInfo, Throwable cause, int retryCount) {
+            return null;
+        }
+
+        @Override
+        public SelectExecutionVerdict onNextPage(Statement<?> statement, String queryInfo) {
+            return null;
+        }
     }
 }
